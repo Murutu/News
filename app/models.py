f60b33429447e9335b97dad4f9db3d13703af452
Murutu <murutupeter@email.com>
2020-01-06 10:56:04 +0300

    Created self.name  in  models.py

--- app/models.py
+++ app/models.py
@@ -2,5 +2,8 @@ class Source:
     '''
     Source class to define Source Objects
     '''
+    def __init__(self, id, name, description, url, category, language, country)
+    self.id = id
+    self.name = name
     
-    def __init__(self, id, name, description,)
+    
