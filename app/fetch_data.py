5d315c81cbf44a549e015b216a72f2cb0d464003
Murutu <murutupeter@email.com>
2020-01-06 09:59:34 +0300

    Added request  in  fetch_data.py

--- app/fetch_data.py
+++ app/fetch_data.py
@@ -1,5 +1,5 @@
 from app import app
-# import requests
+import requests
 import json
 from .models import Source, Article
 
@@ -7,5 +7,8 @@ def get_sources():
     '''
     Function that gets the json response to our url request
     '''
-    sources_url = https://newsapi.org/v2/sources?apiKey=bf2d30bc84e1419abab595739a664dd9
+    sources_url = 'https://newsapi.org/v2/sources?apiKey=apiKey={app.config["NEWS_API_KEY"]}'
+    
+    res = requests.get(sources_url)
+    sources_data
     
