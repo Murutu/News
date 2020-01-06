11226c9ab15c5bbdbb4981765133082c55ffb212
Murutu <murutupeter@email.com>
2020-01-06 12:00:09 +0300

    Created app.route  in  views.py

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from flask import render_template
 from app import app
 from .fetch_data import get_sources, get_articles
+
+@app.route('/')
+def index():
+    sources = get_sources()
+    return render
