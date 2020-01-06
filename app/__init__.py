3f4dcd6f3c4676a2b8a92b6cc8284ef6e4393fc5
Murutu <murutupeter@email.com>
2020-01-06 09:34:08 +0300

    Added class Source in  models.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 
+#Initializing application
 app = Flask(__name__, instance_relative_config=True)
 
-from app import views
-
+#Setting up configuration
 app.config.from_object('config')
+
+from app import views
--- app/fetch_data.py
+++ app/fetch_data.py
@@ -1,1 +1,11 @@
 from app import app
+# import requests
+import json
+from .models import Source, Article
+
+def get_sources():
+    '''
+    Function that gets the json response to our url request
+    '''
+    # sources_url = 
+    
