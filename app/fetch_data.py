e59f15e6fca88ebfb22bea6fe4988d3d7ba8a2de
Murutu <murutupeter@email.com>
2020-01-06 09:56:40 +0300

    Added a link in  fetch_data.py

--- app/fetch_data.py
+++ app/fetch_data.py
@@ -7,5 +7,5 @@ def get_sources():
     '''
     Function that gets the json response to our url request
     '''
-    # sources_url = 
+    sources_url = https://newsapi.org/v2/sources?apiKey=bf2d30bc84e1419abab595739a664dd9
     
