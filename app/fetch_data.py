8046494f84639a0a0053d9462f723b883e2e9938
Murutu <murutupeter@email.com>
2020-01-06 10:19:08 +0300

    Added id  in  fetch_data.py

--- app/fetch_data.py
+++ app/fetch_data.py
@@ -18,4 +18,8 @@ def process_sources(sources_data):
     '''
     Function that converts source dict into source model
     '''
+    sources = []
+    for source_data in sources_data:
+        source = Source(source_data['id'], source_data['name'], source_data['description'], source_data['url'],
+                         source_data['category'], source_data['langauge'], source_data['country'])
     
